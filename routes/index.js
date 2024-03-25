bdc7721b9183001d49e477848326641f6fd11d5a
ChoiJaeWoon <152960408+ChoiJaeWoon@users.noreply.github.com>
2024-03-25 19:58:37 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.html');
-});
+app.use(cors())
 
-module.exports = router;
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/sound/:name', (req, res) => {
+  const { name } = req.params
+
+  if(name == "dog"){
+    res.json({'sound' : '멍멍'})
+  }else if(name == "cat"){
+    res.json({'sound' : '야옹'})
+  }else if(name == "pig"){
+    res.json({'sound' : '꿀꿀'})
+  }else {
+    res.json({'sound' : '알수없음'})
+  }
+})
+
+app.listen(port, () => {
+  console.log('Example app listening on port $(port)')
+})
